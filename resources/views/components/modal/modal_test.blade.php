009be6a675f55db6a0afc683bf7190d53254c9d9
Kristian Tare <kristiankharl.tare@nmsc.edu.ph>
2024-08-02 16:43:22 +0800

    Fixing

--- resources/views/components/modal/modal_test.blade.php
+++ resources/views/components/modal/modal_test.blade.php
@@ -19,7 +19,7 @@
 
 <div id="edit_modal" class="fixed bottom-0 right-0 w-96 bg-white p-4 border border-gray-300 rounded-lg shadow-lg hidden modal-enter">
     <div class="flex justify-between items-center mb-4">
-        <h2 class="text-xl font-semibold">Edit Dvision</h2>
+        <h2 class="text-xl font-semibold">Edit Division</h2>
         <button class="close text-gray-500 hover:text-gray-700">&times;</button>
     </div>
     <div class="grid gap-6 mb-6 md:grid-cols-1">
