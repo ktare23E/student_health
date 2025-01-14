77044ae057ff009c5ea17dfd995858f785379480
Kristian <kristiankharl.tare@nmsc.edu.ph>
2025-01-14 11:21:09 +0800

    debug start

--- resources/views/admin/nurse/index.blade.php
+++ resources/views/admin/nurse/index.blade.php
@@ -34,6 +34,7 @@
                                 </thead>
                                 <tbody>
                                     @foreach ($nurses as $nurse)
+                                    @dd($nurse->entity)
                                         <tr>
                                             <td class="data1">{{ $nurse->last_name }}</td>
                                             <td class="data2">{{ $nurse->first_name }}</td>
